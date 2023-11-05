2a136b7a7c9da5a1e20dddfa87868776bba93a5f
Emerson0199 <148399771+Emerson0199@users.noreply.github.com>
2023-11-05 20:34:19 -0300

    Update AddProf.tsx

--- src/AddProf.tsx
+++ src/AddProf.tsx
@@ -8,6 +8,23 @@ export function AddProf(){
 	<div className="adicionar m-1 col-lg-10 col-md-9 col-sm-8">
         <p className="text fs-3 m-0">
               div de cadastrar professor
+		<head>
+			<div class="table">
+				<table>
+					<thead>
+						<tr>
+							<th>Nome do Professor(a)</th>
+							<th>Turma</th>
+							<th>Turno</th>
+							<th>Horário</th>
+						</tr>
+					</thead>
+				</table>
+			</div>
+	        <body>
+			<h3>Professores cadastrado</h3>
+		</body>  
+		</head>
         </p>
 	</div>
 
