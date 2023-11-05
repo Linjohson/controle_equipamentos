f59509c726484ce77072e2d44a78518deeb05792
Emerson0199 <148399771+Emerson0199@users.noreply.github.com>
2023-11-05 20:23:14 -0300

    Update AddEquip.tsx

--- src/AddEquip.tsx
+++ src/AddEquip.tsx
@@ -4,10 +4,28 @@ import style from '../src/css/Style.module.css';
 
 export function AddEquip(){
 	return(
-
+               
 		<div className="consultar m-1 col-lg-10 col-md-9 col-sm-8">
         	<p className="text fs-3 m-0">
-              div de cadastrar equipamento
+              <div de cadastrar equipamento></div>
+		<head>
+		<div clss="table">
+			<table>
+				<thead>
+					<tr>
+						<th>Código</th>
+						<th>Quantidade</th>
+						<th>Marca</th>
+						<th>Modelo</th>
+						<th>Descrição</th>
+					</tr>
+				</thead>
+			</table>
+		</div>
+		</head>
+		<body>
+			<h3>Cadastrar equipamentos</h3>
+		</body>
             </p>
         </div>
 
